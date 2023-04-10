b96248db3363422394932333bd793fd6c57124c1
Grayson Shogan <graysontoddshogan@gmail.com>
2023-04-09 21:59:49 -0700

    Complete, working on styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage";
-import OrderForm from "./Components/OrderForm";
 import PizzaForm from "./Components/PizzaForm";
+import "./App.css";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/order" element={<OrderForm />} />
-      <Route path="/pizza" element={<PizzaForm />} />
+      <Route path="/order" element={<PizzaForm />} />
     </Routes>
   );
 };
--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -1,5 +1,137 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const toppings = [
+  "Pepperoni",
+  "Mushrooms",
+  "Onions",
+  "Sausage",
+  "Bacon",
+  "Extra Cheese",
+  "Black Olives",
+  "Green Peppers",
+  "Pineapple",
+];
+
 function PizzaForm() {
-  return <form id="pizza-form"></form>;
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [customerOrder, setCustomerOrder] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    if (event.target.value.length < 2) {
+      setNameError("name must be at least 2 characters");
+    } else {
+      setNameError("");
+    }
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    const selectedToppings = toppings.filter((topping) => data[topping]);
+
+    const order = {
+      name: data["name-input"],
+      size: data["size"],
+      toppings: selectedToppings,
+      specialInstructions: data["special-text"],
+    };
+
+    axios
+      .post("https://reqres.in/api/users", order)
+      .then((response) => {
+        setCustomerOrder(response.data);
+      })
+      .catch((error) => {
+        setCustomerOrder("Error submitting your order!", error);
+      });
+  }
+
+  return (
+    <div>
+      <form id="pizza-form" onSubmit={handleSubmit}>
+        <label htmlFor="name-input">
+          {" "}
+          Name for the Order
+          <input
+            type="text"
+            name="name-input"
+            id="name-input"
+            value={name}
+            onChange={handleNameChange}
+          />
+          {nameError && <p className="error-message">{nameError}</p>}
+        </label>
+        <label htmlFor="size-selector">
+          {" "}
+          Choice of Size
+          <select name="size" id="size-dropdown">
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </label>
+        <fieldset>
+          <legend>Choice of Sauce:</legend>
+          <label htmlFor="marinara">
+            Marinara
+            <input
+              id="marinara-sauce"
+              type="radio"
+              name="sauce"
+              value="marinara"
+            />
+          </label>
+          <label htmlFor="alfredo">
+            Alfredo
+            <input
+              id="alfredo-sauce"
+              type="radio"
+              name="sauce"
+              value="alfredo"
+            />
+          </label>
+          <label htmlFor="bbq">
+            BBQ
+            <input id="bbq-sauce" type="radio" name="sauce" value="bbq" />
+          </label>
+        </fieldset>
+        <label htmlFor="toppings">
+          Select your toppings:
+          {toppings.map((topping) => (
+            <div key={topping}>
+              <input type="checkbox" id={topping} name={topping} />
+              <label htmlFor={topping}>{topping}</label>
+            </div>
+          ))}
+        </label>
+        <label htmlFor="special-text">
+          Special Instructions:
+          <input id="special-text" type="text" name="special-text" value="" />
+        </label>
+        <label htmlFor="order-button">
+          <button id="order-button" type="submit">
+            Add to Order
+          </button>
+        </label>
+      </form>
+      {customerOrder && (
+        <div>
+          <p>
+            Order for {customerOrder.name}: {customerOrder.size} pizza with{" "}
+            {customerOrder.toppings}
+          </p>
+          {customerOrder.specialInstructions && (
+            <p>Special instructions: {customerOrder.specialInstructions}</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PizzaForm;
